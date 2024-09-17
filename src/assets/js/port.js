c73f94f4fd14cb38e45b1f577176a4427e3c09c1
xvlelx <xvlelx@naver.com>
2024-09-17 13:05:35 +0900

    마 무 리 완 료

--- src/assets/js/port.js
+++ src/assets/js/port.js
@@ -20,25 +20,3 @@ export function port() {
         },
     });
 }
-
-import ScrollTrigger from "gsap/ScrollTrigger";
-
-export function port() {
-    gsap.registerPlugin(ScrollTrigger);
-    const horSection = gsap.utils.toArray(".port__item");
-
-    gsap.to(horSection, {
-        xPercent: -120 * (horSection.length - 1),
-        ease: "none",
-        scrollTrigger: {
-            trigger: "#port",
-            start: "top 56px",
-            end: "+=3000",
-            pin: true,
-            scrub: 1,
-            markers: false,
-            invalidateOnRefresh: true,
-            anticipatePin: 1,
-        },
-    });
-}
